f198425bb7763e7992253b771064409111448a95
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-01-17 00:39:34 +0000

    First Attempt

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronounOrArticle = ["the", "our", "his", "my", "a"];
+let adjective = ["great", "stupid", "fantastic", "rational", "fancy"];
+let noun = ["monkey", "racoon", "chipmunk", "walrus", "rat"];
+let domain = [".com", ".edu", ".ve", ".gov", ".mx"];
+
+for (let i = 0; i < pronounOrArticle.length; i++) {
+  for (let j = 0; j < adjective.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < domain.length; l++) {
+        console.log(
+          `${pronounOrArticle[i]}${adjective[j]}${noun[k]}${domain[l]}`
+        );
+      }
+    }
+  }
+}
