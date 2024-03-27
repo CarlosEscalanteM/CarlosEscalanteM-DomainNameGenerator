ca6ad80cdbf5ed1dd394a800209498e5bc9d30fe
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-03-27 02:05:44 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronounOrArticle = ["the", "our", "his", "my", "a"];
-let adjective = ["great", "stupid", "fantastic", "rational", "fancy"];
-let noun = ["monkey", "racoon", "chipmunk", "walrus", "rat"];
-let domain = [".com", ".edu", ".ve", ".gov", ".mx"];
+let pronounOrArticle = ["the", "our", "his", "her", "my", "a"];
+let adjective = ["great", "stupid", "fantastic", "rational", "clumsy", "fancy"];
+let noun = ["monkey", "racoon", "chipmunk", "walrus", "rat", "teacher"];
+let domain = [".com", ".edu", ".ve", ".gov", ".mx", "co"];
 
 for (let i = 0; i < pronounOrArticle.length; i++) {
   for (let j = 0; j < adjective.length; j++) {
